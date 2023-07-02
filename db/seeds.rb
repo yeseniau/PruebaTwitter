b2664d2ba65bc9b2139690d28f44d9209aa3ead2
yeseniau <urdaneta.yesenia@gmail.com>
2023-07-02 19:38:33 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+
+25.times do |i|
+    Twitter.create(username: Faker::Artist.name, description: Faker::GreekPhilosophers.quote)
+end
